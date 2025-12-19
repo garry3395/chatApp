96ad437cb3347c40e944aac53e415f9d555f4ec9
Gurjeet Singh <g9876430@gmail.com>
2025-12-19 09:40:27 +0530

    images and sound added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,23 @@
 
-import { Routes ,Route} from 'react-router'
+import { Routes ,Route, Navigate} from 'react-router'
 import ChatPage from './pages/ChatPage'
 import LoginPage from './pages/LoginPage'
 import SignUpPage from './pages/SignUpPage'
 import { useAuthStore } from './store/useAuthStore'
-
+import { useEffect } from 'react'
+import PageLoader from './components/PageLoader'
+import {Toaster} from 'react-hot-toast'
 const App = () => {
 
   
-  const { authUser,login,isLoggedIn} = useAuthStore();
+  const { checkAuth,isCheckingAuth,authUser} = useAuthStore();
+  useEffect(()=>{
+    checkAuth()
+  },[checkAuth])
+  console.log({authUser})
+  if(isCheckingAuth) return <PageLoader/>
 
-       console.log("isLoggedIn",isLoggedIn);
-
-
+     
   return (
    
     
@@ -21,15 +26,15 @@ const App = () => {
       <div className="absolute inset-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px]" />
       <div className="absolute top-0 -left-4 size-96 bg-pink-500 opacity-20 blur-[100px]" />
       <div className="absolute bottom-0 -right-4 size-96 bg-cyan-500 opacity-20 blur-[100px]" />
-  <button onClick={login} className="btn btn-primary z-10">hi</button>  
 
-  
+
+    
 <Routes>
-  <Route path="/"  element={<ChatPage/>} />
-  <Route path="/login"  element={<LoginPage/>} />
-  <Route path="/signup"  element={<SignUpPage/>} />
+  <Route path="/"  element={authUser?<ChatPage/>:<Navigate to={"/login"} />}/>
+  <Route path="/login"  element={!authUser?<LoginPage/>:< Navigate to={"/"}/>} />
+  <Route path="/signup"  element={!authUser?<SignUpPage/>:<Navigate to={"/"}/>} />
 </Routes>
-
+<Toaster/>
 </div>
   )
 }
