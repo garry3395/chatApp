c97d645beb18768b84a570f9d19a9e309ff0f37e
garry3395 <g9876430@gmail.com>
2026-02-27 14:03:17 +0100

    6th try

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -2,17 +2,38 @@ import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
 import { ENV } from './env.js';
 dotenv.config();
-export const generateToken = (userId, res) => {
-  const token = jwt.sign({ id: userId }, ENV.JWT_SECRET, {
-    expiresIn: '7d'
-  });
+/* export const generateToken=(userId,res)=>{
+  const token=jwt.sign({id:userId},ENV.JWT_SECRET,{
+    expiresIn:'7d'
+  })
+  res.cookie('jwt',token,{
+    maxAge:7*24*60*60*1000,
+    httpOnly:true,
+    sameSite:'lax',
+    secure:ENV.NODE_ENV==='development'?false:true,
 
-  res.cookie('jwt', token, {
-    maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
-    httpOnly: true,
-    secure: ENV.NODE_ENV === 'production', // true in production
-    sameSite: 'none', // allow cross-origin cookies
-  });
+   
 
+  })
+ 
   return token;
-};
+  
+} */
+
+export const generateToken=(userId,res)=>{
+  const token=jwt.sign({id:userId},ENV.JWT_SECRET,{
+    expiresIn:'7d'
+  })
+  res.cookie('jwt',token,{
+    maxAge:7*24*60*60*1000,
+    httpOnly:true,
+    sameSite:'lax',
+    secure:true,
+
+   
+
+  })
+ 
+  return token;
+  
+}
