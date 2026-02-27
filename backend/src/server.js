8dc559c4abd2e10aab5c9c1a53433f8fba57b236
garry3395 <g9876430@gmail.com>
2026-02-27 16:21:42 +0100

    1.2 final commit

--- backend/src/server.js
+++ backend/src/server.js
@@ -21,13 +21,13 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
 // production build
-if (ENV.NODE_ENV === "production") {
+/* if (ENV.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
   app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
   });
 }
-
+ */
 const PORT = ENV.PORT || 3000;
 
 server.listen(PORT, () => {
